75df6dfaad17ae6ebbf80a20e51f8aa9e305e3fe
daqingwax <71051492+daqingwax@users.noreply.github.com>
2020-12-29 10:40:12 -0500

    Update Leetcode84.java

--- Leetcode84.java
+++ Leetcode84.java
@@ -65,3 +65,69 @@ class Solution {
         return(res);
     }
 }
+
+//segment tree implementation
+class Solution {
+    private int[] st;
+    public int largestRectangleArea(int[] heights) {
+        //Deque<Integer> dq = new LinkedList<>();
+        int n = heights.length;
+        if(n == 0) return(0);
+        createSegTree(heights);
+        //System.out.println(Arrays.toString(this.st));
+        return(largestRectangleAreaUtil(heights, 0, n - 1));
+    }
+    
+    public int largestRectangleAreaUtil(int[] arr, int l, int r){
+        if(l > r) return(0);
+        if(l == r) return(arr[l]);
+        int m = query(arr, l, r);
+        int res = arr[m] * (r - l + 1);
+        //System.out.printf("l = %d, r = %d, m = %d, res = %d.\n", l, r, m, res);
+        res = Math.max(res, largestRectangleAreaUtil(arr, l, m - 1));
+        res = Math.max(res, largestRectangleAreaUtil(arr, m + 1, r));
+        return(res);
+    }
+    
+    
+    public void createSegTree(int[] arr){
+        int n = arr.length;
+        int depth = (int)Math.ceil(Math.log(n) / Math.log(2));
+        int stSize = (int) Math.pow(2, depth + 1) - 1;
+        this.st = new int[stSize];
+        createSegTreeUtil(arr, 0, n - 1, 0);
+    }
+    public int mid(int x, int y){
+        return(x + (y - x) / 2);
+    }
+    
+    public int merge(int[] arr, int x, int y){
+        if(arr[x] < arr[y]) return(x);
+        return(y);
+    }
+    
+    public int createSegTreeUtil(int[] arr, int ss, int se, int si){
+        if(ss == se){
+            this.st[si] = ss;
+            return(ss);
+        } 
+        int m = mid(ss, se);
+        int left = createSegTreeUtil(arr, ss, m, 2 * si + 1);
+        int right = createSegTreeUtil(arr, m + 1, se, 2 * si + 2);
+        this.st[si] = merge(arr, left, right);
+        return(this.st[si]);
+    }
+    public int queryUtil(int[] arr, int ss, int se, int qs, int qe, int si){
+        if(qs <= ss && se <= qe) return(this.st[si]);
+        int m = mid(ss, se);
+        if(qe <= m) return(queryUtil(arr, ss, m, qs, qe, 2 * si + 1));
+        if(qs > m) return(queryUtil(arr, m + 1, se, qs, qe, 2 * si + 2));
+        return(merge(arr, queryUtil(arr, m + 1, se, qs, qe, 2 * si + 2), queryUtil(arr, ss, m, qs, qe, 2 * si + 1)));
+    }
+    
+    public int query(int[] arr, int qs, int qe){
+        int n = arr.length;
+        if(qs > qe || qs < 0 || qe >= n) return(0);
+        return(queryUtil(arr, 0, n - 1, qs, qe, 0));        
+    }
+}
