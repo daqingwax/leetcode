9ae74b7768daad388061c8f6c9cfad898073d2dd
daqingwax <71051492+daqingwax@users.noreply.github.com>
2020-12-31 19:05:43 -0500

    Update Leetcode494.java

--- Leetcode494.java
+++ Leetcode494.java
@@ -22,3 +22,22 @@ class Solution {
         return(memo[start][currSum + 1000]);
     }
 }
+
+//equal subset dp
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int n = nums.length;
+        int sum = 0;
+        for(int i = 0; i < n; i++) sum += nums[i];
+        if(S > sum || S < -sum || (sum + S) % 2 != 0) return(0);
+        int target = Math.min((sum + S) / 2, (sum - S) / 2);
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for(int i = 0; i < n; i++){
+            for(int j = target; j >= nums[i]; j--){
+                dp[j] += dp[j - nums[i]];
+            }
+        }
+        return(dp[target]);
+    }
+}
